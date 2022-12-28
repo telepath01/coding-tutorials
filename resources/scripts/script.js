868c8199c160027f0f878fbd693dc7e18277f350
telepath01 <47895030+telepath01@users.noreply.github.com>
2022-12-28 12:25:46 -0500

    none

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,12 +1,2 @@
 'use strict'
 
-function Player(name,marker) {
-    this.name = name
-    this.marker = marker
-    this.SayName = function(){
-        console.log(name);
-    }
-    
-}
-const player1 = new Player('steve','X')
-const player2 =new Player('Mark', 'O')
