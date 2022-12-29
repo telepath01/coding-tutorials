2f1bacdbb0bacae62f728532f0abd23e82753a90
telepath01 <47895030+telepath01@users.noreply.github.com>
2022-12-28 20:20:33 -0500

    Update script.js

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,3 +1,15 @@
 "use strict";
 
-let bet = "home";
+function Dog(name, breed, color) {
+  (this.name = name),
+    (this.breed = breed),
+    (this.color = color),
+    (this.bark = function () {
+      return "Woof!";
+    });
+}
+
+console.log(Dog);
+
+const dog1 = new Dog("Daisy", "Labrador", "black");
+console.log(dog1);
