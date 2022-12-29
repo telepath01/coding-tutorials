7b82f10f2c3d2f131af19557b50e5ae5a54fd2f7
telepath01 <47895030+telepath01@users.noreply.github.com>
2022-12-29 07:51:07 -0500

    Update script.js

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,3 +1,20 @@
 "use strict";
 
-let bet = "home";
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    let readOutput = "";
+    if (read === true) {
+      readOutput = "the book has been read.";
+    } else if (!read) {
+      readOutput = "the book has not been read.";
+    }
+    return `${title} by ${author}, ${pages} pages, ${readOutput}`;
+  };
+}
+const theHobbit = new Book("The Hoobit", "J.R.R", 295, false);
+theHobbit.info();
+console.log(theHobbit.constructor);
