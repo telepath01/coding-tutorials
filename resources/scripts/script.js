635e2644678bc4ad89ef59cbf599d10bf41dd5e9
telepath01 <47895030+telepath01@users.noreply.github.com>
2023-01-04 15:11:23 -0500

    Just working on tutorials
    Nothing to update

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,9 +1,27 @@
-"use strict";
-
-const buttons = document.querySelectorAll("button");
-
-buttons.forEach(function (button) {
-  button.addEventListener("click", () => {
-    alert(button.id);
-  });
-});
+'use strict';
+const Player = (name, level) => {
+  let health = level * 2;
+  const getLevel = () => level;
+  const getName = () => name;
+  const die = () => {
+    // oh no
+  };
+  const damage = (x) => {
+    health -= x;
+    if (health <= 0) {
+      die();
+    }
+  };
+  const attack = (enemy) => {
+    if (level < enemy.getLevel()) {
+      damage(1);
+      console.log(`${enemy.getName()} has damaged ${name}`);
+    } else if (level > enemy.getLevel()) {
+      enemy.damage(1);
+      console.log(`${name} has damaged ${enemy.getName()}`);
+    }
+  };
+  return { attack, damage, getLevel, getName };
+};
+const jimmy = Player('jimmy', 5);
+const bob = Player('bob', 3);
