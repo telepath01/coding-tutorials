84cced24a99ab49df6ad1e0164f4160acfd74bc6
telepath01 <47895030+telepath01@users.noreply.github.com>
2023-01-12 12:07:12 -0500

    working with classes

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,27 +1,10 @@
 'use strict';
-const Player = (name, level) => {
-  let health = level * 2;
-  const getLevel = () => level;
-  const getName = () => name;
-  const die = () => {
-    // oh no
-  };
-  const damage = (x) => {
-    health -= x;
-    if (health <= 0) {
-      die();
-    }
-  };
-  const attack = (enemy) => {
-    if (level < enemy.getLevel()) {
-      damage(1);
-      console.log(`${enemy.getName()} has damaged ${name}`);
-    } else if (level > enemy.getLevel()) {
-      enemy.damage(1);
-      console.log(`${name} has damaged ${enemy.getName()}`);
-    }
-  };
-  return { attack, damage, getLevel, getName };
-};
-const jimmy = Player('jimmy', 5);
-const bob = Player('bob', 3);
+
+class User {
+  constructor(name) {
+    this.name = name;
+  }
+  sayHi() {
+    alert(this.name);
+  }
+}
